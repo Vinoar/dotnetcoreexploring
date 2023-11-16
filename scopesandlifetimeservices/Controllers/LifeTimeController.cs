5918e92edfe8587b76d4eb9b940d52552d89d8c4
Vinoth Arun Raj Xavier <vxavier1@ebintl.com>
2023-11-16 17:00:37 +0530

    Changes on results

--- scopesandlifetimeservices/Controllers/LifeTimeController.cs
+++ scopesandlifetimeservices/Controllers/LifeTimeController.cs
@@ -25,17 +25,37 @@ namespace scopesandlifetimeservices.Controllers
             _sample = sample;
         }
 
-        [HttpGet("request")]
-        public ActionResult RequestMade()
+        [HttpGet("process")]
+        public ActionResult Process()
         { 
-            var message = $"<!DOCTYPE html><html lang=en><meta charset=UTF-8><meta content=\"width=device-width,initial-scale=1\"name=viewport><title>Scopes and Lifetime Management</title><h1>Scopes and Lifetime Management.</h1>" +
-                $"GetSingletonID<ul><li><b>Sample Service </b>{_sample.GetSingletonID()}<li><b>ISingletonService Service</b> {_singletonService.SingletonId}</ul>" +
-                $"GetScopedID<ul><li><b>Sample Service </b>{_sample.GetScopedID()}<li><b>IScopedService Service </b>{_scopedService.ScopeId}</ul>" +
-                $"GetTransientID<ul><li><b>Sample Service </b>{_sample.GetTransientID()}<li><b>ITransientService Service</b> {_transientService.TransientId}</ul>";
+            var message =
+                $"<div class='single'> GetSingletonID <ul><li><b>Sample Service </b>{_sample.GetSingletonID()}<li><b>ISingletonService Service</b> {_singletonService.SingletonId}</ul></div>" +
+                $"<div class='scope'>GetScopedID <ul><li><b>Sample Service </b>{_sample.GetScopedID()}<li><b>IScopedService Service </b>{_scopedService.ScopeId}</ul></div>" +
+                $"<div class='transient'>GetTransientID <ul><li><b>Sample Service </b>{_sample.GetTransientID()}<li><b>ITransientService Service</b> {_transientService.TransientId}</ul></div>";
 
             return new ContentResult
             {
                 Content = message,
+                ContentType = "text/html"
+            };
+        }
+
+        [HttpGet("request")]
+        public async Task<ActionResult> RequestMade()
+        {
+
+            using HttpClient client = new HttpClient();            
+            var responseMessage = await client.GetAsync("https://localhost:44365/api/LifeTime/process");
+
+            using HttpClient client1 = new HttpClient();
+            var responseMessage1 = await client1.GetAsync("https://localhost:44365/api/LifeTime/process");
+            using HttpClient client2 = new HttpClient();
+
+            var responseMessage2 = await client2.GetAsync("https://localhost:44365/api/LifeTime/process");
+            return new ContentResult
+            {
+                Content = "<!DOCTYPE html><html lang=en><meta charset=UTF-8><meta content=\"width=device-width,initial-scale=1\"name=viewport><title>Scopes and Lifetime Management</title><center><h1>Scopes and Lifetime Management</h1></center> <style>.single{border: 3px red solid;margin: 5px;padding: 5px;}.scope{border: 3px green solid;margin: 5px;padding: 5px;}.transient{border: 3px blue solid;margin: 5px;padding: 5px;}</style>" +
+                $"<table><tr><td>{responseMessage.Content.ReadAsStringAsync().Result}</td><td> {responseMessage1.Content.ReadAsStringAsync().Result}</td><td> {responseMessage2.Content.ReadAsStringAsync().Result}</td></tr></table>",
                 ContentType = "text/html"
             };
         }
